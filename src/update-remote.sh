4ebf0bce7141a3dfbf9816c9d77c8b31c948b871
rmn@mbp <rohoehn123@gmail.com>
2022-01-13 01:48:25 +0100

    Thu Jan 13 01:48:25 CET 2022: update-remote.sh

--- src/update-remote.sh
+++ src/update-remote.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-DIR="~/github"
-cd ${DIR}
+DIR="/Users/rmn/github"
+cd $DIR
 
+LIST=$(cat remote.txt)
 
-
-for REPO in remote.txt
+for REPO in $LIST
     do
         cd ${REPO}
         echo "Updating repository ${REPO}"
         git pull
-        git add * & git commit -m "$(date): update-remote.sh" & git push
+        git add *; git commit -m "$(date): update-remote.sh"; git push
         cd ${DIR}
     done
 
