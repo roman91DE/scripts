ea499f7590ab2b22f81608c03da71332baf5df30
rmn@mbp <rohoehn123@gmail.com>
2022-01-13 02:17:57 +0100

    Thu Jan 13 02:17:57 CET 2022: update-remote.sh

--- src/update-remote.sh
+++ src/update-remote.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-DIR="/Users/rmn/github"
-REMOTE_FILE="${DIR}/remote.txt"
+# set parent directory for remote repositories
+PROJECT_DIR="/Users/rmn/github"
 
-cd $DIR
+# list all remote repositories to be included in file remote.txt inside PROJECT_DIR
+REMOTE_FILE="${PROJECT_DIR}/remote.txt"
+
+cd $PROJECT_DIR
 
 if [ ! -f "$REMOTE_FILE" ]; then
-    echo "Create remote.txt as a list of remote repositories"
+    echo "ERROR - Create remote.txt as a list of remote repositories"
     exit
 fi
-
 
 LIST=$(cat remote.txt)
 
@@ -17,9 +19,8 @@ for REPO in $LIST
     do
         cd ${REPO}
         echo "Updating repository ${REPO}"
+        echo "-------------------------------"
         git pull
         git add *; git commit -m "$(date): update-remote.sh"; git push
-        cd ${DIR}
+        cd ${PROJECT_DIR}
     done
-
-
